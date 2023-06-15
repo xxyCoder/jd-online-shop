453d7ba1f854000dad4abe03c1c1745b3c67d487
xxy <1696944145@qq.com>
2023-06-15 16:13:09 +0800

    修改Bug

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import store from './store';
 import router from './router';
 import {
     ElMain,
@@ -33,7 +32,6 @@ import 'element-plus/dist/index.css';
 const app = createApp(App);
 
 app
-    .use(store)
     .use(router)
     .use(ElContainer)
     .use(ElHeader)
